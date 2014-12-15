46b5a39c3fe5ef69e03b72c7c77149d2b69e1067
Amir Yasin <ayasin@datatensor.com>
2014-12-14 19:26:10 -0600

    Added ability to look up routes

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var FRHttp = require('./lib/frhttp.js');
 
 var server = FRHttp.createServer();
 
-server.GET('/api/client/:clientId').log();
-server.GET('/api/client/:clientId/package/:package').log();
+server.GET('/api/client/:clientId');
+server.GET('/api/client/:clientId/package/:package').onValue(
+	function() {
+		server.TAP_GET('/api/client/123/package/34').log();
+		server.TAP_GET('/api/client/123').log();
+	}
+);
