d28d17a8c22556dce61d318bd3324b1e200e724f
Amir Yasin <ayasin@datatensor.com>
2014-12-14 21:52:09 -0600

    Beginnings of a runnable route

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var server = FRHttp.createServer();
 server.GET('/api/client/:clientId');
 server.GET('/api/client/:clientId/package/:package').onValue(
 	function() {
-		server.TAP_GET('/api/client/123/package/34').log();
+		server.TAP_GET('/api/client/123/package/34').onValue(function (val) {
+			console.log(JSON.stringify(val.toObject().URLVariables));
+		});
 		server.TAP_GET('/api/client/123').log();
 	}
 );
