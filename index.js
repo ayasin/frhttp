175d1f7bc699c77842c108bb2f71fb72fbc05db5
Amir Yasin <ayasin@datatensor.com>
2014-12-16 19:45:40 -0600

    More work on route creation and start of execute phases.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ server.GET('/api/client/:clientId');
 server.GET('/api/client/:clientId/package/:package').onValue(
 	function() {
 		server.TAP_GET('/api/client/123/package/34').onValue(function (val) {
-			console.log(JSON.stringify(val.toObject().URLVariables));
+			console.log(JSON.stringify(val));
 		});
 		server.TAP_GET('/api/client/123').log();
 	}
