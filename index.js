a8118d4730153b92ffb2cbb4f16a58777aaede8a
Amir Yasin <ayasin@datatensor.com>
2015-01-11 00:51:34 -0600

    Basic hello world works!!

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 'use strict';
+
+
 var FRHttp = require('./lib/frhttp.js');
-var http = require('http');
-/*
 var server = FRHttp.createServer();
 
-server.GET('/api/client/:clientId');
-server.GET('/api/client/:clientId/package/:package').onValue(
-	function() {
-		server.TAP_GET('/api/client/123/package/34').onValue(function (val) {
-			console.log(JSON.stringify(val));
-		});
-		server.TAP_GET('/api/client/123').log();
-	}
-);
-*/
+server.GET('/api/multiply/:first/:second').onValue(function (path) {
+		path.process.on(
+			{
+				name: 'multiply',
+				enter: null,
+				exit: null,
+				params: ['request:url_vars'],
+				produces: ['mul'],
+				fn: function (produce, input) {
+					produce.value('mul', input['request:url_vars'].first * input['request:url_vars'].second);
+					produce.done();
+				}
+			}
+		).render(
+			{
+				params: ['mul'],
+				fn: function(writer, input) {
+					writer.write(input.mul);
+					writer.done();
+				}
+			}
+		);
+	});
+
+server.listen(8009);
+
 
 /*
-http.createServer(function (req, res) {
-	server.findAndExecute(req, res);
-}).listen(8080);
-*/
+var Bacon = require('baconjs');
 
-var bacon = require('baconjs');
+var a = new Bacon.Bus();
+var b = new Bacon.Bus();
+
+var temp = Bacon.combineTemplate({
+	a : a,
+	b: a.sampledBy(b)
+}).endOnError().onValue(function() {console.log('I was called'); return 33;}).onError(function (e) { console.log(e); });
+
+a.push(15);
+b.push(13);
+a.end();
+b.error('there was a problem');
+*/
