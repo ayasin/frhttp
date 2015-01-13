9446244078ff65baa7316844425e6cdc7837cf3c
Amir Yasin <ayasin@datatensor.com>
2015-01-13 00:23:43 -0600

    Added another sample.  Fixed running count error when enter filters out a value.

--- index.js
+++ index.js
@@ -103,4 +103,43 @@ server.POST('/api/replay').onValue(function (path) {
 	});
 });
 
+server.GET('/api/factorial/:number').onValue(function (route) {
+	route.process.on({
+		name: 'setup',
+		params: [server.CONSTANTS.URL_VARS],
+		produces: ['max', 'total'],
+		fn: function(produce, input) {
+			produce.value('max', +input[server.CONSTANTS.URL_VARS].number);
+			produce.value('total', {count: 1, current: 1});
+			produce.done();
+		}
+	}).on({
+		name: 'calculate',
+		params: ['max', 'total'],
+		produces: ['total'],
+		takeMany: true,
+		enter: function(input) {
+			if (input.total.count > input.max) {
+				return undefined;
+			}
+			return input;
+		},
+		fn: function(produce, input) {
+			var ret = {
+				count: input.total.count + 1,
+				current: input.total.current * input.total.count
+			};
+			produce.value('total', ret);
+			produce.done();
+		}
+	}).render(
+		{
+			params: ['max', 'total'],
+			fn: function(writer, input) {
+				writer.writeBody(String(input.total.current));
+			}
+		}
+	)
+});
+
 server.listen(8001);
