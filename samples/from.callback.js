68408bccec0df193018e5637e67139c286c9f45e
Amir Yasin <ayasin@datatensor.com>
2015-02-21 19:44:55 -0600

    move from callback sample to short form when and render.

--- samples/from.callback.js
+++ samples/from.callback.js
@@ -3,14 +3,15 @@ var fs = require('fs');
 
 function createRoute(server) {
 	server.GET('/samples/from.callback').onValue(function (route) {
-		route.when('stat this file',[],['fstat'],function (producer) {
-			producer.fromNodeCallback(['fstat'], -1, fs.stat, null, './samples/from.callback.js');
-		}).render({
-			params: ['fstat'],
-			fn: function(writer, input) {
+		route.when('stat this file',[],['fstat'],
+			function (producer) {
+				producer.fromNodeCallback(['fstat'], -1, fs.stat, null, './samples/from.callback.js');
+			}
+		).render(['fstat'],
+			function(writer, input) {
 				writer.writeBody(input.fstat);
 			}
-		});
+		);
 	});
 }
 
