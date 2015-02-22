77fc2eb092babf2d277d3df41508ef2e9b3ea84c
Amir Yasin <ayasin@datatensor.com>
2015-02-21 19:42:58 -0600

    use types to detect difference between when being called with a full obj, vs short form.  Do the same for render.

--- samples/from.callback.js
+++ samples/from.callback.js
@@ -3,13 +3,8 @@ var fs = require('fs');
 
 function createRoute(server) {
 	server.GET('/samples/from.callback').onValue(function (route) {
-		route.when({
-			name: 'stat this file',
-			params: [],
-			produces: ['fstat'],
-			fn: function (producer) {
-				producer.fromNodeCallback(['fstat'], -1, fs.stat, null, './samples/from.callback.js');
-			}
+		route.when('stat this file',[],['fstat'],function (producer) {
+			producer.fromNodeCallback(['fstat'], -1, fs.stat, null, './samples/from.callback.js');
 		}).render({
 			params: ['fstat'],
 			fn: function(writer, input) {
